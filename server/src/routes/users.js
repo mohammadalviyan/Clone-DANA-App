84f1e09cc7309f6cf0e9bccc3a31e4cfaaca501d
nur hasan <nhasan716@gmail.com>
2019-11-19 01:03:33 +0700

    change response

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,14 +1,15 @@
 const Router = require('express');
 const router = Router();
 const usersController = require("../controllers/users.controller");
+const verify = require('../middleware/verifyToken');
+
 
 router
   .post("/register", usersController.createUsers)
   .post("/login", usersController.usersLogin)
   .post("/checkusers", usersController.checkNumber)
   .post("/otp/signup", usersController.otpSignup)
-  .get('/test',
-    (req, res) => res.send('Hello World!')
-  )
+  // .get("/",verify,usersController.getAllUsers)
+  // .patch("/:number",usersController.updateProfile)
 
 module.exports = router;
