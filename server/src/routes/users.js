089565ad36d30bd9d084d9c25b39d477552eebef
Mohammad Alviyan Anwari <mohammadalviyan17@gmail.com>
2019-11-20 12:27:06 +0700

    fixed conflict user routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,12 +1,8 @@
 const Router = require('express');
 const router = Router();
 const usersController = require("../controllers/users.controller");
-<<<<<<< HEAD
 const { upload } = require('../middleware/uploadImage');
-=======
 const verify = require('../middleware/verifyToken');
-
->>>>>>> fa1519780c4e033545aa656301e76f7b385012a0
 
 router
   .post("/register",upload.single("image"), usersController.createUsers)
