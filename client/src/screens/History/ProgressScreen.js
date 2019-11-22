1824acf3b43bb52c62745ed68638267bc45d404b
Arian Derida <arianderida@gmail.com>
2019-11-23 05:17:27 +0700

    LogOut

--- client/src/screens/History/ProgressScreen.js
+++ client/src/screens/History/ProgressScreen.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const ProgressScreen = () => {
+const ProgressScreen = (props) => {
+    
+    const {resultLogin} = useSelector(state => state.auth);
+
+    const deleteToken = async () => {
+      try {
+        await AsyncStorage.removeItem('user');
+        props.navigation.navigate('AuthScreen');
+      } catch (err) {
+        console.log(`The error is: ${err}`);
+      }
+    };
+
+    // console.log(resultLogin)
     return (
-        <View>
-            <Text>ASD</Text>
-        </View>
-    )
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+          onPress={() => deleteToken()}
+          style={{backgroundColor: 'red', width: 100}}>
+          <Text>ASD</Text>
+        </TouchableOpacity>
+      </View>
+    );
 };
 
 export default ProgressScreen;
